3e9f9bebfe4fa6788bf68f7bc8f659d1830053a8
Asim Maharjan <maharjan291@gmail.com>
2018-01-06 20:59:46 +0545

    Every thing civilized

--- program2.c
+++ program2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int greatestCommonDivisor(int,int);
 void main(){
-	int hcf  = greatestCommonDivisor(6,8);
-	printf("The gcd of 6 and 8 is %d\n",hcf);
+	int hcf  = greatestCommonDivisor(0,6);
+	printf("The gcd of 0 and 6 is %d\n",hcf);
 }
 
 int greatestCommonDivisor(int m,int n){
