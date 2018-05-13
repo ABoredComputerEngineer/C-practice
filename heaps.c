6083f565e4db5e89a0a6cab8798213e7183dc2da
Asim Maharjan <maharjan291@gmail.com>
2018-05-13 19:33:37 +0545

    UI is working.. need to build heap while enqueuing

--- heaps.c
+++ heaps.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <stdbool.h>
 
+#define PQ_LENGTH 1000 // the maximum length of the queue
+#define LIMIT 100 // the maximum number of characters for input
+#define TABLE_SIZE 100 // the max index of the hash table
 #define parent(x) ( ( (x) == 0 )?-1:( ((x) - 1) / 2 ) )
 #define lchild(x) ( 2 * (x) + 1 )
 
@@ -11,40 +14,51 @@ typedef struct {
      int length;
 } priorityQueue;
 
+enum command { add = 9, extract = 91, show = 65, fill = 39 , quit = 67, sort = 72 , build = 48};
 
+/* ..... . ARRAY RELATED FUNCTIONS ........
+ *   fillArray fills the array with random numbers. it takes in array and its length
+ *   displayArray prints the array.it takes in array and its length. */
 int *fillArray(int *, int length);
 int *displayArray(int *, int length);
 
+/*... QUEUE RELATED FUCNTIONS .... */
+void enqueue(priorityQueue*, int); // adds an item in the priority queue
+void dequeue(priorityQueue*);
+
+/*.....   HEAP RELATED FUNCTIONS ..... */
 void buildMaxHeap(priorityQueue *);
 void maxHeapify(priorityQueue *, int);
-void heapSort(priorityQueue *);
+void heapSort(priorityQueue );
 int extractMax(priorityQueue *);
-int *displayTree(int *, int); // displays an array as a binary tree
+
+/* ... DICTIONARY / HASH TABLE RELATED FUNCTIONS .. */
+int hash(char *); // takes a string and produces a hash
+
+
+/*.... MISCALLENEOUS FUNCTIONS .... */
 void swap(int *, int *);
-int loga2(int); // calculates the integer part of log2
-int power(int, int);
+int getString(char *);
+int readInput(char *, priorityQueue *);
+int strToInt(char *);
+int isDigit(char);
+
 int main(void){
      priorityQueue q1;
-     int i;
-     q1.arr = (int *)malloc(sizeof(int)*10);
-     q1.length = 10;
-     for ( i = 0; i < q1.length ; i++ )
-         q1.arr[i] =  i ;
-     for(i = 0; i < q1.length; i++ )
-         printf("%d ",q1.arr[i] );
+     int i,exit;
+     char *command = (char *)malloc(sizeof(char) * LIMIT );
+     srand(time(NULL));
 
-     buildMaxHeap(&q1);
-     putchar('\n'); 
-     for(i = 0; i < q1.length; i++ )
-         printf("%d ",q1.arr[i] );
-     putchar('\n');
+     q1.arr = (int *)malloc(sizeof(int)*PQ_LENGTH);
+     q1.length = 0;
 
-     displayTree(q1.arr, q1.length);
-     heapSort(&q1);
-     for(i = 0; i < q1.length; i++ )
-         printf("%d ",q1.arr[i] );
-     putchar('\n');
-     q1.length = 10;
+     do {
+          getString(command);
+          exit = readInput(command, &q1);
+     } while ( exit != true );     
+
+     free(q1.arr);
+     free(command);
      return 0; 
 }
 
@@ -78,13 +92,114 @@ int extractMax(priorityQueue *new){
 
 }
 
-void heapSort(priorityQueue *new ){
-     
-     while ( new->length > 0 )
-          printf("%d\t",extractMax(new) );
+void heapSort(priorityQueue new ){
+     while ( new.length > 0 )
+          printf("%d\t",extractMax(&new) );
 
      
      return;
+}
+
+void enqueue(priorityQueue *new, int value){
+     new->arr[new->length] = value;
+     new->length++;
+     maxHeapify(new,parent(new->length));
+     return;
+}
+
+void dequeue(priorityQueue *new){
+     extractMax(new);
+     return;
+}
+
+
+int getString(char *x){
+     unsigned int i = 0;
+     unsigned char c;
+     while ( ( c = getchar() ) != '\n' && i < LIMIT )
+          x[i++] = c;
+     x[i] = 0;
+     if ( c !=  '\n')
+          while( ( c = getchar() != '\n') );      
+     return i;
+}
+
+int strToInt(char *x){
+     int i = 0;
+     int multiplier = 1, num = 0, sign = 1, rev = 0;
+
+     // removing preceding blanks
+     while ( x[i] == ' ' )
+          i++;
+
+     // reading the sign( -ve or +ve ) of the number
+     if ( x[i] == '-' ){
+          sign = -1;
+          i++;
+     }
+
+     while ( x[i] == '0' )  // remove preceding zeros 
+          i++;
+
+     while ( ( x[i] != 0 ) && isDigit(x[i] ) ){
+          num = num * multiplier + ( x[i] - '0' ) ;
+          multiplier*=10; 
+          i++;
+     } 
+     
+     return sign*num;
+
+}
+
+int readInput(char *x, priorityQueue *new){
+     char command[10], value[LIMIT]; 
+     int i = 0,j,info;
+     
+     while ( x[i] == ' ') i++;
+     for ( j = 0; x[i] != 0 &&  x[i] != ' ' ; i++ )
+          command[j++] = x[i];
+     command[j] = 0;
+     
+     for ( j = 0; x[i] != 0 ; i++ )
+          value[j++] = x[i];
+     value[j] = 0;
+
+     info = strToInt(value);
+
+     switch ( hash(command) ){
+          case add:
+               enqueue(new,info);          
+               break;
+          case extract:
+               dequeue(new);
+               break;
+          case sort:
+               heapSort(*new);
+               break;
+          case fill:
+               fillArray(new->arr,new->length);
+               break;
+          case show:
+               displayArray(new->arr,new->length);
+               break;
+          case build:
+               buildMaxHeap(new);
+               break;
+          case quit:
+               return true;
+               break;
+          default:
+               break;
+     }
+
+     return false;
+}
+
+int hash(char *x){
+     int sum = 0, i = 0;
+     while ( x[i] != 0 )
+          sum += ( x[i++] - 'a' + 1);
+     return sum;
 }
 
 void swap(int *x, int  *y){
@@ -93,47 +208,22 @@ void swap(int *x, int  *y){
      *x = temp;
 }
      
-     
-int *displayTree(int *arr, int length){
-     int i,j,temp = loga2( length ), k = 0, space,l;
-     int height = ( power(2,temp) == length )?temp:(temp+1);
-
-     for ( i = 0; i < length ; ){
-          for ( space = height; space >= 0; space--)
-               putchar(' ');
-          temp = power(2,k);
-          for ( j = 0; j < temp  && i < length; j++ ){
-               printf("%d ",arr[i++]);
-          }
-          putchar('\n');
-
-          height--;
-
-          k++;
-     }
+int *fillArray(int *x, int length){
+     int i;
+     for ( i = 0; i < length ; i++ )
+          x[i] = rand()%301 ;
+     return x;
+}     
 
 
-     return arr;
-
+int *displayArray(int *x, int length){
+     int i;
+     for ( i = 0; i < length; i++ )
+          printf("%d ",x[i]);
+     putchar('\n');
+     return x;
 }
 
-int loga2(int x){
-     if ( x == 1 )
-          return 0;
-     else 
-          return 1 + loga2(x/2);
-
+int isDigit( char x ){
+     return ( x >= '0' && x <= '9' )?true:false;
 }
-     
-int power(int base, int exp){
-     if ( exp == 0 )
-          return 1;
-     else
-          return base*power(base,exp-1);
-}
-
-
-
-
-
-
